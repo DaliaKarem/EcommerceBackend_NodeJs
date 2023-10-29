91c35c92170527a116840c2eb9481cbb552c5d36
Dalia Karem <daliakarem0027@gmail.com>
2023-10-29 17:30:59 +0200

    Error Handling

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const morgan=require('morgan')//middleware logger
 dotenv.config({path:'config.env' })
 const DB=require('./config/DB')
 const categoryRouter=require('./Routes/categoryRoute')
+const ApiError = require('./utils/apiError')
 
 //connect DB
 DB()
@@ -16,6 +17,15 @@ app.use(morgan('dev'))
 
 //Routes
 app.use("/api/v1/categories",categoryRouter)
+app.all('*',(req,res,next)=>{
+    next(new ApiError(`Error ${req.originalUrl} Doesn't Exist`,400))
+})
+//Error Handler with Express Middleware
+app.use((err,req,res,next)=>{
+
+    res.status(err.statusCode).json({Error:err,massege:err.massege,stack:err.stack})
+})
+
 
 app.listen(process.env.PORT,()=>{
     console.log(`Running Done on port ${process.env.PORT}`)
