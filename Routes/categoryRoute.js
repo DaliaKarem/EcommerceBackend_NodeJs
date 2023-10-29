1355c2099d1a9877d518c2df8939aaa5e5b7ca71
Dalia Karem <daliakarem0027@gmail.com>
2023-10-29 17:02:07 +0200

    Delete Category

--- Routes/categoryRoute.js
+++ Routes/categoryRoute.js
@@ -1,9 +1,9 @@
-const {addCategory,getAllCategories,getSpacificCategory}=require('../services/categoryServices')
+const {addCategory,getAllCategories,UpdateSpacificCategory,getSpacificCategory,DeleteSpacificCategory}=require('../services/categoryServices')
 const express=require('express')
 const router=express.Router()
 //router.post("/",addCategory)
 router.route("/").post(addCategory).get(getAllCategories)
-router.route("/:id").get(getSpacificCategory)
+router.route("/:id").get(getSpacificCategory).put(UpdateSpacificCategory).delete(DeleteSpacificCategory)
 
 //router.get("/",getAllCategories)
 module.exports=router
--- services/categoryServices.js
+++ services/categoryServices.js
@@ -37,3 +37,35 @@ exports.getSpacificCategory=asyncHandler(async(req,res)=>{
     res.status(200).json({data:Cate}) 
 
   })
+
+//des   update spacificCategory
+//route  PUT /api/v1/categories/:id
+//acc    Admin(private)
+exports.UpdateSpacificCategory=asyncHandler(async(req,res)=>{
+    const {id}=req.params;
+    const {name}=req.body;
+    const Cate=await CateModel.findByIdAndUpdate({_id:id},{name,slug:slugify(name),},{update:true}); 
+    if(!Cate)
+    {
+        res.status(404).json({msg:`Error There is no Category With this ID ${id}`}) 
+
+    }
+    res.status(200).json({data:Cate}) 
+
+  })
+ 
+
+//des   Delete spacificCategory
+//route  Delete /api/v1/categories/:id
+//acc    Admin(private)
+exports.DeleteSpacificCategory=asyncHandler(async(req,res)=>{
+    const {id}=req.params;
+    const Cate=await CateModel.findByIdAndDelete(id); 
+    if(!Cate)
+    {
+        res.status(404).json({msg:`Error There is no Category With this ID ${id}`}) 
+
+    }
+    res.status(204).json({msg:`Done...Delete Category With this ID ${id}`}) 
+
+  })  
