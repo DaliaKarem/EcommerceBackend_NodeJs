184352af8df2854fe9367899059c89559177bfee
Dalia Karem <daliakarem0027@gmail.com>
2023-11-02 13:38:12 +0200

    All validatation layer

--- Routes/categoryRoute.js
+++ Routes/categoryRoute.js
@@ -1,10 +1,10 @@
 const {addCategory,getAllCategories,UpdateSpacificCategory,getSpacificCategory,DeleteSpacificCategory}=require('../services/categoryServices')
 const express=require('express');
-const {validateSpecificCateg}=require('../utils/validators/Categoryvalidator');
+const {validateSpecificCateg,validateAddCateg,validateDeleteCateg,validateUpdateCateg}=require('../utils/validators/Categoryvalidator');
 const router=express.Router()
 //router.post("/",addCategory)
-router.route("/").post(addCategory).get(getAllCategories)
-router.route("/:id").get(validateSpecificCateg,getSpacificCategory).put(UpdateSpacificCategory).delete(DeleteSpacificCategory)
+router.route("/").post(validateAddCateg,addCategory).get(getAllCategories)
+router.route("/:id").get(validateSpecificCateg,getSpacificCategory).put(validateUpdateCateg,UpdateSpacificCategory).delete(validateDeleteCateg,DeleteSpacificCategory)
 
 //router.get("/",getAllCategories)
 module.exports=router
--- utils/validators/Categoryvalidator.js
+++ utils/validators/Categoryvalidator.js
@@ -5,3 +5,19 @@ exports.validateSpecificCateg=[
     check('id').isMongoId().withMessage('Invalid Id'),
      validatorMiddelWare,
 ]
+
+exports.validateAddCateg=[
+    check('name').notEmpty().withMessage("Name Req").isLength({min:3}).withMessage("To Short").isLength({max:30}).withMessage("To long"),
+     validatorMiddelWare,
+]
+
+exports.validateUpdateCateg=[
+    check('id').isMongoId().withMessage('Invalid Id'),
+     validatorMiddelWare,
+]
+
+
+exports.validateDeleteCateg=[
+    check('id').isMongoId().withMessage('Invalid Id'),
+     validatorMiddelWare,
+]
